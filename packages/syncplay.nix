399b42cdb4c6f804536fe0855b3841eb5afbe6a7
NextFire <git@yuru.moe>
2024-04-04 21:33:19 -0400

    syncplay: remove all patches

--- packages/syncplay.nix
+++ packages/syncplay.nix
@@ -3,11 +3,10 @@
 }:
 syncplay.overrideAttrs (oldAttrs: rec {
   name = "${pname}-${version}";
-
   pname = "syncplay";
   version = builtins.substring 0 7 syncplay-src.rev;
 
   src = syncplay-src;
 
-  patches = builtins.tail oldAttrs.patches;
+  patches = [ ];
 })
